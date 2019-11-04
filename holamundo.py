8cb0b734cf83cf98502b3af0cad6b0fa90007443
962245899 <57157985+962245899@users.noreply.github.com>
2019-11-03 20:38:46 -0500

    calculadoras en python

---
+++ holamundo.py
@@ -0,0 +1,317 @@
+#ejercicio 1
+#calcular el area del cuadrado
+area=0.0
+base=0.0
+altura=0.0
+
+#asignacion de valores
+base=5
+altura=5
+
+#calculo
+area=(base*altura)
+
+#mostrar valores
+print("base= " , base)
+print("altura=" , altura)
+print("area=", area)
+
+
+#ejercicio 2
+#gastos del desayuno
+avena=0.0
+pan=0.0
+mantequilla=0.0
+gasto_total=0.0
+#asignacion de valores
+avena=1.50
+pan=1.0
+mantequilla=1.0
+#calculo
+gasto_total=(avena+pan+mantequilla)
+#mostrar valores
+print("avena=" , avena)
+print("pan=", pan)
+print("mantequilla=", mantequilla)
+print("gasto_total=", gasto_total)
+
+#ejercicio 3
+#medida de angulos internos del triangulo equilatero
+a=0.0
+b=0.0
+c=0.0
+suma_total=0.0
+#asignacion de valores
+a=60
+b=60
+c=60
+#calculo
+suma_total=(a+b+c)
+#mostrar valores
+print("a=", a)
+print("b=", b)
+print("c=", c)
+print("suma_total=", suma_total)
+#ejercicio 4
+#medida de angulos externos
+x=0.0
+y=0.0
+z=0.0
+medida_de_angulos_externos=0.0
+#asignacion de valores
+x=120
+y=120
+z=120
+#calculo
+medida_de_angulos_externos=(x+y+z)
+#mostrar valores
+print("x=", x)
+print("y=", y)
+print("z=", z)
+print("medida_de_angulos_externos=",medida_de_angulos_externos)
+#ejercicio 5
+#botellas de agua consumidas
+botellas_de_agua_al_dia=0
+total=0
+dias_de_consumo=0
+#asignacion de valores
+botellas_de_agua_al_dia=1
+dias_de_consumo=20
+#calculo
+total=(botellas_de_agua_al_dia+dias_de_consumo)
+#mostrar valores
+print("botellas_de_agua_al_dia", botellas_de_agua_al_dia)
+print("dias_de_consumo=", dias_de_consumo)
+print("total=",total)
+#ejercicio 6
+#cuadernos utilizados
+cuaderno_por_curso=0
+cursos=0
+total_de_cursos=0
+#asignacion de valores
+cuaderno_por_curso=1
+cursos=6
+#calculo
+total_de_cursos=(cuaderno_por_curso*cursos)
+#mostrar valores
+print("cuaderno_por_curso=", cuaderno_por_curso)
+print("cursos=", cursos)
+print("total_de_cursos=", total_de_cursos)
+#ejercicio 7
+#metros de tela usado
+cantidad_a=0.0
+cantidad_b=0.0
+cantidad_c=0.0
+total_de_tela=0.0
+#asignacion de valores
+cantidad_a=1.40
+cantidad_b=1.50
+cantidad_c=1.10
+#calculo
+total_de_tela=(cantidad_a+cantidad_b+cantidad_c)
+#mostrar valores
+print("cantidad_a=", cantidad_a)
+print("cantidad_b=", cantidad_b)
+print("cantidad_c=", cantidad_c)
+print("total_de_tela=", total_de_tela)
+
+#ejercicio 8
+#promedio de notas
+nota_1=0.0
+nota_2=0.0
+nota_3=0.0
+promedio=0.0
+#asignacion de valores
+nota_1=19.5
+nota_2=20
+nota_3=18.5
+#calculo
+promedio=((nota_1+nota_2+nota_3)/3)
+#mostrar valores
+print("nota_1=", nota_1)
+print("nota_2=", nota_2)
+print("nota_3=", nota_3)
+print("promedio=", promedio)
+#ejercicio 9
+#trabajos pendientes
+trabajo_1=0
+trabajo_2=0
+total_de_trabajos=0
+#asignacion de valores
+trabajo_1=1
+trabajo_2=3
+#calculo
+total_de_trabajos=(trabajo_1+trabajo_2)
+#mostrar valores
+print("trabajo_1=", trabajo_1)
+print("trabajo_2=", trabajo_2)
+print("total_de_trabajos=", total_de_trabajos)
+#ejercicio 10
+#dinero ahorrado
+dia_1=0.0
+dia_2=0.0
+dia_3=0.0
+total_ahorrado=0.0
+#asignacion de valores
+dia_1=3.00
+dia_2=3.50
+dia_3=2.50
+#calculo
+total_ahorrado=(dia_1+dia_2+dia_3)
+#mostrar valores
+print("dia_1=", dia_1)
+print("dia_2=", dia_2)
+print("dia_3=", dia_3)
+print("total_ahorrado=", total_ahorrado)
+#ejercicio 11
+#usb utilizados por ciclo
+ciclo_1=0
+ciclo_2=0
+total_de_usb=0
+#asignacion de valores
+ciclo_1=1
+ciclo_2=0
+#calculo
+total_de_usb=(ciclo_1+ciclo_2)
+#mostrar valores
+print("ciclo_1=", ciclo_1)
+print("ciclo_2=", ciclo_2)
+print("total_de_usb=", total_de_usb)
+#ejercicio 12
+#suma de a?os de la familia
+edad_1=0
+edad_2=0
+edad_3=0
+total_de_anios=0
+#asignacion de valores
+edad_1=53
+edad_2=50
+edad_3=18
+#calculo
+total_de_anios=(edad_1+edad_2+edad_3)
+#mostrar valores
+print("edad_1=", edad_1)
+print("edad_2=", edad_2)
+print("edad_3=", edad_3)
+print("total_de_anios=", total_de_anios)
+#ejercicio 13
+#alumnos de facfym 2019II
+electronica=0
+informatica=0
+total_de_alumnos=0
+#asignacion de valores
+electronica=42
+informatica=18
+#calculo
+total_de_alumnos=(electronica+informatica)
+#mostrar valores
+print("electronica=", electronica)
+print("informatica=", informatica)
+print("total_de_alumnos=", total_de_alumnos)
+#ejercicio 14
+#obras leidas
+primaria=0
+secundaria=0
+total_de_obras=0
+#asignacion de valores
+primaria=15
+secundaria=43
+#calculo
+total_de_obras=(primaria+secundaria)
+#mostrar valores
+print("primaria=", primaria)
+print("secundaria=", secundaria)
+print("total_de_obras=", total_de_obras)
+#ejercicio 15
+#formula mru
+velocidad=""
+distancia=""
+tiempo=""
+#asignacion de valores
+velocidad="v"
+tiempo="t"
+#calculo
+distancia="(velocidad*tiempo)"
+#mostrar valores
+print("velocidad=", velocidad)
+print("tiempo=", tiempo)
+print("distancia=", distancia)
+
+#ejercicio 16
+#trabajadores de un colegio
+administrativos=0
+profesores=0
+total_de_trabajadores=0
+#asignacion de valores
+administrativos=16
+profesores=54
+#calculo
+total_de_trabajadores=(administrativos+profesores)
+#mostrar valores
+print("administrativos=", administrativos)
+print("profesores=", profesores)
+print("total_de_trabajadores=", total_de_trabajadores)
+
+#ejercicio 17
+#salario de trabajo
+tiempo_trabajando=0
+dias_enfermo=0
+total_de_dias_trabajado=0
+#asignacion de valores
+tiempo_trabajando=12
+dias_enfermo=2
+#calculo
+total_de_dias_trabajado=(tiempo_trabajando-dias_enfermo)
+#mostrar valores
+print("tiempo_trabajando=", tiempo_trabajando)
+print("dias_enfermo=", dias_enfermo)
+print("total_de_dias_trabajado=", total_de_dias_trabajado)
+
+#ejercicio 18
+#sacos de arroz vendidos
+sacos_de_arroz=0
+precio_de_saco=0
+recaudado=0
+#asignacion de valores
+sacos_de_arroz=700
+precio_de_saco=115
+#calculo
+recaudado=(sacos_de_arroz*precio_de_saco)
+#mostrar valores
+print("sacos_de arroz=", sacos_de_arroz)
+print("precio_de_saco=", precio_de_saco)
+print("recaudado=", recaudado)
+
+#ejercicio 19
+#terreno en km
+terreno_actual=0
+terreno_por_comprar=0
+total_del_terreno=0
+#asignacion de valores
+terreno_actual=150
+terreno_por_comprar=90
+#calculo
+total_del_terreno=(terreno_actual+terreno_por_comprar)
+#mostrar valores
+print("terreno_actual=", terreno_actual)
+print("terreno_por_comprar=", terreno_por_comprar)
+print("total_del_terreno=", total_del_terreno)
+
+
+#ejercicio 20
+#gastos de utiles de aseo
+jabon=0
+shampoo=0
+cepillo=0
+total_de_gastos=0
+#asignacion de valores
+jabon=2.0
+shampoo=3.20
+cepillo=3.30
+#calculo
+total_de_gastos=(jabon+shampoo+cepillo)
+#mostrar valores
+print("jabon=", jabon)
+print("shampoo=", shampoo)
+print("cepillo=", cepillo)
+print("total_de_gastos=", total_de_gastos)
